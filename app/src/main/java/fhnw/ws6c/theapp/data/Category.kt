65afecb9cece43b27ca6e32d5abdd2000e8af4fa
Jenny Tran <puijanjenny.tran@students.fhnw.ch>
2021-04-19 17:49:43 +0200

    added category and remotecategoryservice

--- app/src/main/java/fhnw/ws6c/theapp/data/Category.kt
+++ app/src/main/java/fhnw/ws6c/theapp/data/Category.kt
@@ -1,4 +1,22 @@
 package fhnw.ws6c.theapp.data
+import org.json.JSONObject
 
-class Category {
+class Category(json: JSONObject) {
+    val drinks = json.getJSONObject("drinks")
+
+    val name = drinks.getString("strDrink")
+    val img = drinks.getString("strDrinkThumb")
+    val id = drinks.getInt("idDrink")
+
+    constructor(jsonString: String) : this(JSONObject(jsonString))
+
+    fun asJson(): String {
+        return "{" +
+                "\"drinks\": $drinks," +
+                "\"strDrink\": \"$name\"," +
+                "\"strDrinkThumb\": \"$img\"," +
+                 "\"idDrink\": \"$id\"," +
+                "}" +
+                "}"
+    }
 }
--- app/src/main/java/fhnw/ws6c/theapp/data/RemoteCategoryService.kt
+++ app/src/main/java/fhnw/ws6c/theapp/data/RemoteCategoryService.kt
@@ -1,8 +1,32 @@
 package fhnw.ws6c.theapp.data
 
+import org.json.JSONObject
+import java.net.URL
+
 class RemoteCategoryService {
 
-    fun requestCategoryList(url : String) : String {
-        return ""
+    val baseURL = "https://www.thecocktaildb.com"
+    val baseSearchURL = "api/json/v1/1/filter.php?c="
+    val baseSearchWord = "Cocktail"
+
+    fun searchAPIRequest(searchParam: String) : String {
+        var requestURL = URL("$baseURL/$baseSearchURL$baseSearchWord")
+        return requestURL.readText()
     }
+
+    fun loadCategory(jsonObject: JSONObject) : List<Category> {
+        var data = jsonObject.optJSONArray("data")
+        var categoryList = ArrayList<Category>()
+
+        if(data == null) {
+            return categoryList
+        }
+
+        for(x in 0 until data.length()) {
+            var CategoryModel = Category(data.getJSONObject(x))
+            categoryList.add(CategoryModel)
+        }
+        return categoryList
+    }
+
 }
