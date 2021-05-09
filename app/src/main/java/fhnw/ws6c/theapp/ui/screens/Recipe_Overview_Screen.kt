1e7121d352d7481604ef5aab6a0fd8190a14ed28
LouisaRe <louisa.reinger@gmx.de>
2021-05-10 00:23:29 +0200

    bug fix import Brush

--- app/src/main/java/fhnw/ws6c/theapp/ui/screens/Recipe_Overview_Screen.kt
+++ app/src/main/java/fhnw/ws6c/theapp/ui/screens/Recipe_Overview_Screen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
